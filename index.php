b1769e8d5912061ec954900c150121466edb2417
Oliver August Åsnes <oliverasnes@gmail.com>
2026-03-10 13:24:45 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Velkommen til CRM løsningen vår</h1>
+    <p><a href="kunder.php">Se liste over våre kunder</a></p>
+    <p><a href="create.php">Se liste over våre ansatte og kontaktpersoner <a href="kontakt.php">Her</a></a></a></p>
+</body>
+</html>
