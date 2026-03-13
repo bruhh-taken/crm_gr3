ef8443e3bca797c1e76fc8b6cd06dac22a3ce5c8
oliver12345678goodboy <oliverasnes@gmail.com>
2026-03-13 09:10:56 +0100

    Inline styles and disable external stylesheet
    Commented out the external style.css link in kunde/kunde.php and added an inline <style> block with layout and table/input/button styles (margins, table borders, hover state, input and button styling) to make the page render with the intended styling immediately. Also updated kunde/style.css to adjust body margins (left/right 30%).

--- kunde/kunde.php
+++ kunde/kunde.php
@@ -25,7 +25,7 @@ $result = $conn->query($sql);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="style.css">
+    <!-- <link rel="stylesheet" href="style.css"> -->
 </head>
 <body>
     <form method="GET">
@@ -58,6 +58,53 @@ $result = $conn->query($sql);
         }
         
         ?>
-    
+        
+<style>
+body {
+    margin-top: 2%;
+    margin-left: 20%;
+    margin-right: 20%;
+}
+
+table {
+    border-collapse: collapse;
+    width: 100%;
+}
+
+th, td {
+    border: 1px solid black;
+    padding: 8px;
+    text-align: left;
+}
+
+th {
+    background-color: #f2f2f2;
+}
+
+tr:hover {
+    background-color: #f5f5f5;
+}
+
+input[type="text"] {
+    padding: 6px;
+    margin-top: 8px;
+    font-size: 17px;
+    border: 1px solid #333333;
+}
+
+button {
+    padding: 8px;
+    margin-top: 8px;
+    margin-left: 4px;
+    margin-bottom: 10px;
+    background-color: rgb(214, 214, 214);
+    color: rgb(22, 22, 22);
+    font-weight: bold;
+    border: solid black 1px;
+    cursor: pointer;
+}
+
+</style>
+
 </body>
 </html>
