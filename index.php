3002ff18bbe8867161d7d74a1914117092359c16
Trevor <trevor.post07@gmail.com>
2026-03-24 13:23:40 +0100

    css

--- index.php
+++ index.php
@@ -18,59 +18,12 @@ if (!isset($_SESSION["bruker"])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hovedside</title>
+    <link rel="stylesheet" href="include/style.css">
 </head>
-<body>
-    <a href="ansatt/"><button>ansatt</button></a>
-    <a href="firma/"><button>firma</button></a><br>
-    <a href="login.php?logout=1">Logg ut</a><br>
-    <a href="login.php?registrer=1">Legg til ny bruker</a>
-
-
-    <style>
-        body {
-            margin-top: 2%;
-            margin-left: 20%;
-            margin-right: 20%;
-        }
-
-        table {
-            border-collapse: collapse;
-            width: 100%;
-        }
-
-        th, td {
-            border: 1px solid black;
-            padding: 8px;
-            text-align: left;
-        }
-
-        th {
-            background-color: #f2f2f2;
-        }
-
-        tr:hover {
-            background-color: #f5f5f5;
-        }
-
-        input[type="text"] {
-            padding: 6px;
-            margin-top: 8px;
-            font-size: 17px;
-            border: 1px solid #333333;
-        }
-
-        button {
-            padding: 8px;
-            margin-top: 8px;
-            margin-left: 4px;
-            margin-bottom: 10px;
-            background-color: rgb(214, 214, 214);
-            color: rgb(22, 22, 22);
-            font-weight: bold;
-            border: solid black 1px;
-            cursor: pointer;
-        }
-
-    </style>
+<body class="container">
+    <a href="ansatt/"><button class="knappindex">ansatt</button></a>
+    <a href="firma/"><button class="knappindex">firma</button></a><br>
+    <a href="login.php?logout=1"><button class="knappindex">Logg ut</button></a><br>
+    <a href="login.php?registrer=1"><button class="knappindex">Legg til ny bruker</button></a>
 </body>
 </html>
