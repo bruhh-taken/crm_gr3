5a5bc9f47a71e11cf8735a94c5a5489f4b71431f
Trevor <trevor.post07@gmail.com>
2026-03-27 09:15:48 +0100

    overføring tilim porsgrunn

--- include/db.php
+++ include/db.php
@@ -1,14 +1,14 @@
 <!-- laget av trevor -->
 <?php
-// $host = "localhost:3306";
-// $user = "db_im24tre1402"; // Brukernavnet til databasen
-// $password = "7y_PERm7idzqd4%h"; // Ditt passord
-// $db = "db_im24tre1402";  // Navnet på databasen
+$host = "localhost:3306";
+$user = "db_im24tre1402"; // Brukernavnet til databasen
+$password = "7y_PERm7idzqd4%h"; // Ditt passord
+$db = "db_im24tre1402";  // Navnet på databasen
 
-$host = "localhost";
-$user = "root"; // Brukernavnet til databasen
-$password = ""; // Ditt passord
-$db = "crm_gr3";  // Navnet på databasen
+// $host = "localhost";
+// $user = "root"; // Brukernavnet til databasen
+// $password = ""; // Ditt passord
+// $db = "crm_gr3";  // Navnet på databasen
 
 // Oppretter en ny kobling til MySQL-databasen
 $conn = new mysqli($host, $user, $password, $db);
